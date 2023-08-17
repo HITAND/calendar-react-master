e2faa750ad9d09afcce4920a926702f7763f3ac6
HITAND <polishball.pl12@gmail.com>
2023-08-17 16:18:27 +0300

    new commit

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-
-import { days } from '../../utils/dateUtils.js';
+import React from "react";
+import { days, formatMins } from "../../utils/dateUtils.js";
 
 const Navigation = ({ weekDates }) => {
+  const currentDate = new Date();
+
   return (
     <header className="calendar__header">
-      {weekDates.map((dayDate) => (
-        <div className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
-        </div>
-      ))}
+      {weekDates.map((dayDate) => {
+        const isCurrentDay =
+          dayDate.getDate() === currentDate.getDate() &&
+          dayDate.getMonth() === currentDate.getMonth() &&
+          dayDate.getFullYear() === currentDate.getFullYear();
+
+        return (
+          <div
+            className={`calendar__day-label day-label ${
+              isCurrentDay ? "current-day" : ""
+            }`}
+          >
+            <span className="day-label__day-name">
+              {days[dayDate.getDay()]}
+            </span>
+            <span className="day-label__day-number">{dayDate.getDate()}</span>
+          </div>
+        );
+      })}
     </header>
   );
 };
